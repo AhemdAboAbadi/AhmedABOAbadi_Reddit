db9628c743da1cdb4446358d7528b11eed0debdc
AhemdAboAbadi <ahmed.free.py@gmail.com>
2021-08-29 00:57:43 +0300

    front end

--- public/js/dom.js
+++ public/js/dom.js
@@ -5,3 +5,15 @@ btnLogSign.addEventListener('click', (e) => {
   console.log('ssss');
   window.location.href = './signup.html';
 });
+
+const popUpTop = document.querySelector('.pop_up_to_top');
+window.addEventListener('scroll', () => {
+  popUpTop.classList.toggle('active', window.scrollY > 900);
+});
+
+popUpTop.addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  });
+});
